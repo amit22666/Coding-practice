343f636fad92a2785f825b84c1c734b65f2c75cb
Amit kumar <38582958+amit22666@users.noreply.github.com>
2021-02-16 14:13:27 +0530

    Add files via upload

---
+++ template/array template/t and n as input.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> //works like magic, but at cost.
+using namespace std;
+
+
+
+int main(){
+    long long int t; cin>>t;
+    
+    while(t--){
+        long long int n; cin>>n;
+        long long int a[n];
+
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+    
+        
+        }
+return 0;
+}
